eca3cf77ffa0171c6578bb0a68e06f304481b766
Hunter <hschafer@uw.edu>
2016-08-15 00:04:06 -0700

    Added SQL database connection for blog

--- server.js
+++ server.js
@@ -6,10 +6,20 @@
 
 import express from 'express';
 import helmet from 'helmet';
+import mysql from 'mysql';
 import path from 'path';
+
+var connectionPool = mysql.createPool({
+  connectionLimit : 100,
+  host     : 'localhost',
+  user     : 'guest',
+  password : 'Passw0rd!',
+  database : 'not_imdb',
+  debug    :  false
+});
+
 const app = express();
 app.use(helmet());
-
 
 /**
  * Anything in public can be accessed statically without
@@ -20,11 +30,36 @@ app.use(express.static(path.join(__dirname, '..', 'public'), {
   dotfiles: 'ignore'
 }));
 
+app.get('/api/query', function(req, res) {
+  connectionPool.getConnection(function(err, connection) {
+    if (err) {
+      connection.release();
+      res.json({"code": 100, "status": "Could not connect to database"});
+      return;
+    }
+    console.log("Query: " + req.query.q);
+    connection.query(req.query.q, function(err, rows) {
+      connection.release();
+      if (err) {
+        res.json({"code": 500, "status": "Error processing query: '" + err + "'"})
+      }
+
+      res.json(rows);
+      return;
+    });
+
+    console.log("Connected to DB with id: " + connection.threadId);
+    connection.on('error', function(err) {
+      res.json({"code" : 100, "status" : "Error in connection database: '" + err + "'"});
+      return;
+    });
+  })
+})
+
 
 /**
  * Always serve the same HTML file for all requests
  */
-
 app.get('*', function(req, res, next) {
   console.log('Request: [GET]', req.originalUrl)
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
@@ -34,7 +69,6 @@ app.get('*', function(req, res, next) {
 /**
  * Error Handling
  */
-
 app.use(function(req, res, next) {
   console.log('404')
   let err = new Error('Not Found');
@@ -50,7 +84,6 @@ app.use(function(err, req, res, next) {
 /**
  * Start Server
  */
-
 const port = 80;
 app.listen(port);
 
