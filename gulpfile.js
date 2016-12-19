148cb076b2a3b9f8670cbe4b1bcd327be5d3d763
Hunter Schafer <hschafer@uw.edu>
2016-12-19 10:52:54 -0800

    Small clean up to gulp file wasn't included in last commit

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('clean:build', function() {
     del('./public/js/*')
 })
 
-gulp.task('build', ['clean:build'], function() {
+gulp.task('serve:build-app', ['clean:build'], function() {
   return gulp.src('./app/app.js')
     .pipe(webpack(webpackConfig))
     .on('error', function handleError() {
@@ -24,33 +24,20 @@ gulp.task('build', ['clean:build'], function() {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('watch:build', function() {
-  return gulp.watch('./app/**/*', ['build']);
+gulp.task('dev:watch', function() {
+  gulp.watch('./app/**/*', ['serve:build-app']);
+  gulp.watch('./public/css/*.css', ['serve:build-app']);
+  gulp.watch('./server.js', ['serve:build-server']);
 });
 
-
-/**
- * Node Server (Express)
- */
-
-gulp.task('serve:node', function(done) {
-  var babelPath = path.join(__dirname, 'node_modules/.bin/babel-node');
-  nodemon({
-    exec: babelPath + ' ./server.js',
-    watch: ['server.js'],
-    ext: 'js html'
-  });
-});
-
-
-gulp.task('serve-test:build', function(done) {
+gulp.task('serve:build-server', function(done) {
   return gulp.src('./server.js')
     .pipe(babel())
     .pipe(gulp.dest('./lib'));
   
 });
 
-gulp.task('serve-test:node', function(done) {
+gulp.task('serve:node', ['serve:build-app', 'serve:build-server'], function(done) {
   nodemon({
     exec: 'node ./lib/server.js',
     watch: ['./lib/server.js'],
